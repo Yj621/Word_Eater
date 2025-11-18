9d02ad1e823e25620f93f4bd40280458118f249c
Yj621 <wjddbswl26@gmail.com>
2025-11-18 13:37:30 +0900

    키보드 모드 변경

--- Assets/02. Scripts/UI/KeyboardAvoider.cs
+++ Assets/02. Scripts/UI/KeyboardAvoider.cs
@@ -14,29 +14,22 @@ public class KeyboardAvoider : MonoBehaviour
     void Update()
     {
 #if UNITY_EDITOR
-        // 에디터에서는 키보드가 없으니 항상 원래 위치
         _rect.offsetMin = _originalOffsetMin;
         return;
 #endif
 
-        // 모바일에서만 동작
         if (TouchScreenKeyboard.visible)
         {
-            // 키보드가 차지하는 영역 (스크린 픽셀 단위)
-            Rect kbArea = TouchScreenKeyboard.area;
+            var kb = TouchScreenKeyboard.area;
+            float h = kb.height;
 
-            // Screen Space - Overlay 캔버스라면 픽셀 == UI 단위라 그대로 사용 가능
-            float keyboardHeight = kbArea.height;
-
-            // 아래쪽 여백만 키보드 높이만큼 올려줌
             _rect.offsetMin = new Vector2(
                 _originalOffsetMin.x,
-                _originalOffsetMin.y + keyboardHeight
+                _originalOffsetMin.y + h
             );
         }
         else
         {
-            // 키보드 내려가면 원래 위치로
             _rect.offsetMin = _originalOffsetMin;
         }
     }
