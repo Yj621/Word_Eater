dc609f33c249e6741f65a69a4a11e651188e668b
Yj621 <wjddbswl26@gmail.com>
2026-01-18 14:39:42 +0900

    [FIX] 광고 계정 변경 및 테스트 ID로 변경

--- Assets/02. Scripts/IAA/AdsManager.cs
+++ Assets/02. Scripts/IAA/AdsManager.cs
@@ -8,9 +8,9 @@ public class AdsManager : MonoBehaviour
     public static AdsManager Instance { get; private set; }
 
 #if UNITY_ANDROID
-    [SerializeField] private string rewardedAdRevivalId = "ca-app-apub-1881501262849586/3221896109"; // 테스트용
+    [SerializeField] private string rewardedAdRevivalId = "ca-app-pub-3940256099942544/5354046379"; // 테스트용
 #elif UNITY_IOS
-    [SerializeField] private string rewardedAdUnitId = "ca-app-pub-3940256099942544/1712485313"; // 테스트용
+    [SerializeField] private string rewardedAdUnitId = "cca-app-pub-3940256099942544/6978759866"; // 테스트용
 #else
     [SerializeField] private string rewardedAdUnitId = "unused";
 #endif
@@ -55,7 +55,7 @@ public class AdsManager : MonoBehaviour
 
         var request = new AdRequest();
         
-        /*
+        
         RewardedAd.Load(rewardedAdRevivalId, request, (RewardedAd ad, LoadAdError error) =>
         {
             if (error != null)
@@ -68,7 +68,7 @@ public class AdsManager : MonoBehaviour
             HookRewardedEvents(_rewardedAd);
             Debug.Log("[Ads] Rewarded loaded.");
         });
-        */
+        
     }
 
     private void HookRewardedEvents(RewardedAd ad)
--- Assets/02. Scripts/IAA/GameReviveSystem.cs
+++ Assets/02. Scripts/IAA/GameReviveSystem.cs
@@ -63,6 +63,9 @@ public class GameReviveSystem : MonoBehaviour
             noThanksText: "아니오"
         );
 
+        // revivePopup이 광고 모드인지 명시적으로 설정 (다른 곳에서 SetAdMode(false)로 바뀌었을 수 있음)
+        revivePopup.SetAdMode(true);
+
         revivePopup.YesNoPanelShow(
             onAccept: () =>
             {
