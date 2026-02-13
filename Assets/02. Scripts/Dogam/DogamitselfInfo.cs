a88703a63a7d1bd42a0f72e2c4c75322ee05a57d
Lim_Dong_Hyeon <151642091+saru033@users.noreply.github.com>
2026-02-13 15:49:12 +0900

    도감 안본애들 검정으로
    업데이드 :
    - 도감의 본 적 없는 워드이터 이미지에 자물쇠를 제거하고 검정색으로 보이게 만들었습니다
    - 자모 선택권 폴더에 있던 개웃긴 버튼을 숨김 처리했습니다

--- Assets/02. Scripts/Dogam/DogamitselfInfo.cs
+++ Assets/02. Scripts/Dogam/DogamitselfInfo.cs
@@ -5,15 +5,24 @@ public class DogamitselfInfo : MonoBehaviour
 {
     public string sprid;
     public bool isOn = false; // 본 적 있는 애인지 확인하는 함수
+    Image img;
+    void Awake()
+    {
+        // 체인 이미지 숨기고
+        Transform chain = transform.Find("Lock_Image");
+        chain.gameObject.SetActive(false);
+
+        // 검은색으로
+        img = GetComponent<Image>();
+        img.color = Color.black;
+    }
 
     public void DogamOn() {
         if (isOn) {
-            // 체인 이미지 숨기고
-            Transform chain = transform.Find("Lock_Image");
-            chain.gameObject.SetActive(false);
+            //흰 색으로
+            img.color = Color.white;
 
             // 메테리얼 제거
-            Image img = GetComponent<Image>();
             img.material = null;
         }
     }
